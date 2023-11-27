b872b84354d7fb2b0a34573168e3f9a2d2a4955b
DHuy <huynguyen07080401@gmail.com>
2023-11-27 15:20:28 +0700

    @h@

--- source/src/middleware/data_nonload.js
+++ source/src/middleware/data_nonload.js
@@ -1,21 +1,21 @@
-// const express = require('express')
-// const router = express.Router();
-// const frontendhelper = require('../helpers/frontendhelper')
+const express = require('express')
+const router = express.Router();
+const frontendhelper = require('../helpers/frontendhelper')
 
 
-// module.exports = async (req, res, next) => {
-//     await frontendhelper.getMenu(req).then((result) => {
-//         res.locals.listMenu = result.data
-//         res.locals.search = ''
-//     })
-//     await frontendhelper.getSetting(req).then((result) => {
-//         res.locals.listSetting = result.dataSetting
-//     })
-//     await frontendhelper.getCategory(req).then((result) => {
-//         res.locals.listCategory = result.data
-//     })
-//     await frontendhelper.getProductCategory(req).then((result) => {
-//         res.locals.listProductCategory = result.data
-//     })
-//     next()
-//   };
+module.exports = async (req, res, next) => {
+    await frontendhelper.getMenu(req).then((result) => {
+        res.locals.listMenu = result.data
+        res.locals.search = ''
+    })
+    await frontendhelper.getSetting(req).then((result) => {
+        res.locals.listSetting = result.dataSetting
+    })
+    await frontendhelper.getCategory(req).then((result) => {
+        res.locals.listCategory = result.data
+    })
+    await frontendhelper.getProductCategory(req).then((result) => {
+        res.locals.listProductCategory = result.data
+    })
+    next()
+  };
